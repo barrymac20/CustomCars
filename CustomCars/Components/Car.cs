23a7028c3ac3be1689e3842c6a781000c9d96049
Barry <Barry@DESKTOP-DQCSI4O>
2024-12-06 20:41:50 +0000

    Add project files.

---
+++ CustomCars/Components/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace CustomCars.Components
+{
+    public enum CarType
+    {
+        Hatchback,
+        Crossover,
+        Saloon,
+        Convertible,
+        SUV,
+        Pickup,
+        MPV,
+        Estate
+    }
+
+    public abstract class Car
+    {
+        // Methods
+        public CarType CarType { get; set; }
+        public string Description { get; set; } = "Car: ";
+        public decimal Cost { get; set; }
+    }
+
+    public abstract class CarDecorator: Car
+    {
+        public abstract string GetDescription();
+        public abstract string GetCost();
+    }
+}
+
+
---
+++ CustomCars/Components/Hatchback.cs
@@ -0,0 +1,22 @@
+﻿namespace CustomCars.Components
+{
+    public class Hatchback : Car
+    {
+        public Hatchback()
+        {
+            CarType = CarType.Hatchback;
+            Description += "Hatchback";
+            Cost = 10000;
+        }
+    }
+
+    public class Crossover : Car
+    {
+        public Crossover()
+        {
+            CarType = CarType.Crossover;
+            Description += "Crossover";
+            Cost = 15000;
+        }
+    }
+}
