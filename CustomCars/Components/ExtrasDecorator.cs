124c258d212bff2202c82aa3702fdce67c7e8af3
Barry <Barry@DESKTOP-DQCSI4O>
2024-12-07 21:14:07 +0000

    Add extrasdecorator. Add some basic css to create a grid system and group decorator options into cards

---
+++ CustomCars/Components/ExtrasDecorator.cs
@@ -0,0 +1,45 @@
+﻿namespace CustomCars.Components
+{
+    public class HeatedSeatsDecorator: CarDecorator
+    {
+        public Car Car { get; set; }
+
+        public HeatedSeatsDecorator(Car car)
+        {
+            Car = car;
+            Description = GetDescription();
+        }
+
+
+        public override decimal GetCost()
+        {
+            return Car.GetCost() + 2500;
+        }
+
+        public override string GetDescription()
+        {
+            return $"{Car.Description} with heated seats";
+        }
+    }
+
+    public class TouchScreenDecorator : CarDecorator
+    {
+        public Car Car { get; set; }
+
+        public TouchScreenDecorator(Car car)
+        {
+            Car = car;
+            Description = GetDescription();
+        }
+
+        public override decimal GetCost()
+        {
+            return Car.GetCost() + 1500;
+        }
+
+        public override string GetDescription()
+        {
+            return $"{Car.Description}, touch screen controls";
+        }
+    }
+}
